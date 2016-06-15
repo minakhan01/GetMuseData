336eb3497e077f4d2f252e4b8b8e8bfc88a7aff6
minakhan01 <minakhan01@gmail.com>
2016-06-15 10:58:50 -0400

    start unity project to get EEG data from Muse

---
+++ Assets/AndroidReceiveMessages.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidReceiveMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AndroidJNI.AttachCurrentThread();
+		AndroidJNIHelper.debug = true; 
+		using (AndroidJavaClass jc = new AndroidJavaClass("com.mina.myapplication.MainActivity")) { 
+			jc.CallStatic("UnitySendMessage", "Main Camera", "JavaMessage", "whoowhoo"); 
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		int RValue = 0;
+//		using (AndroidJavaClass javaClass = new AndroidJavaClass("com.mina.myapplication.MainActivity")) {
+//			using (AndroidJavaObject activity = javaClass.GetStatic<AndroidJavaObject>("mContext"))
+//			{
+//				Debug.Log("rate :"+activity.CallStatic<float>("getRate"));
+//			}
+//		}
+//
+//		//		Debug.Log("rate" + RValue.ToString() );
+	}
+
+	void receiveAlpha(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveEeg(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveAccel(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveStatus(string message) {
+		Debug.Log("message from java: " + message);
+	}
+}
